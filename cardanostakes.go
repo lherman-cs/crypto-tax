5f0ca2c09bc601a9fdb506351649104b55570e51
Lukas Herman <lherman.cs@gmail.com>
2024-03-23 22:02:42 -0400

    Add cardano readme for external hdd

--- cardanostakes.go
+++ cardanostakes.go
@@ -3,3 +3,4 @@ package main
 // https://developers.cardano.org/docs/integrate-cardano/creating-wallet-faucet/
 // use cardano full node wallet, then query the address
 //
+// https://iohk.zendesk.com/hc/en-us/articles/900004340586-How-to-symlink-Daedalus-chain-folder
